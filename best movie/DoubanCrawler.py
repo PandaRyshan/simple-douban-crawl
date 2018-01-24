9c209a6ea63bc7a9502d543363cc2fe67fddb0d4
aold619 <aold619@gmail.com>
2018-01-24 18:13:52 +0800

    finished get and write the data

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -1,25 +1,24 @@
+"""
+get the movie list on movie.douban.com.
+"""
 import sys
-import time
 import requests
-import lxml
-import expanddouban
+import csv
 from bs4 import BeautifulSoup
-from selenium import webdriver
+import expanddouban
 
 
-"""
-return a string corresponding to the URL of douban movie lists given category and location.
-"""
-
-
-def get_movie_url(category, location):
+def get_movie_url(category="", location=""):
     """ return a string corresponding to the URL of douban movie lists given category and location.
     arguments:
     category -- movie's category str()
     location -- movie's location str()
     """
-    url = "https://movie.douban.com/tag/#/?sort=S&tags=电影"
-    url += ",{},{}".format(category, location)
+    url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影"
+    if category.__ne__(""):
+        url += ",{}".format(category)
+    if location.__ne__(""):
+        url += ",{}".format(location)
     return url
 
 
@@ -36,7 +35,7 @@ class Movie(object):
     __info_link = ""
     __cover_link = ""
 
-    def __init__(self, name, rate, location, category, info_link, cover_link):
+    def __init__(self, name="", rate="", location="", category="", info_link="", cover_link=""):
         self.__name = name
         self.__rate = rate
         self.__location = location
@@ -48,41 +47,92 @@ class Movie(object):
         """get name"""
         return self.__name
 
+    def set_name(self, name):
+        """set name"""
+        self.__name = name
+
     def get_rate(self):
         """get rate"""
         return self.__rate
+
+    def set_rate(self, rate):
+        """set rate"""
+        self.__rate = rate
 
     def get_location(self):
         """get location"""
         return self.__location
 
+    def set_location(self, location):
+        """set location"""
+        self.__location = location
+
     def get_category(self):
         """get category"""
         return self.__category
+
+    def set_category(self, category):
+        """set category"""
+        self.__category = category
 
     def get_info_link(self):
         """get info_link"""
         return self.__info_link
 
+    def set_info_link(self, info_link):
+        """set info_link"""
+        self.__info_link = info_link
+
     def get_cover_link(self):
         """get cover_link"""
         return self.__cover_link
 
+    def set_cover_link(self, cover_link):
+        """set cover_link"""
+        self.__cover_link = cover_link
 
 # m = Movie("name", "rate", "location", "category", "info_link", "cover_link")
 # print(m.get_name())
 
 
-def get_movies(category, location):
+def get_movies(category="", location=""):
     """get the movie list you want"""
+    movie_list = []
+    movie = None
+
     url = get_movie_url(category, location)
-    html = expanddouban.getHtml(url)
-    with open(sys.path[0] + "\\output.txt", "w", encoding="utf-8") as target:
-        target.write(html)
-    return []
+    html = expanddouban.getHtml(url, loadmore=True)
+    soup = BeautifulSoup(html, "html.parser")
+    tag_list = soup.find(id="content").find(class_="list-wp").find_all("a")
+
+    for tag in tag_list:
+        movie = Movie()
+        movie.set_name(str(tag.p.find(class_="title").string))
+        movie.set_rate(str(tag.p.find(class_="rate").string))
+        movie.set_info_link(str(tag.get("href")))
+        movie.set_cover_link(str(tag.find("img").get("src")))
+        movie.set_category(category)
+        movie.set_location(location)
+        movie_list.append(movie)
+
+    return movie_list
 
 
-get_movies("喜剧", "大陆")
+with open(sys.path[0] + "\\movies.csv", "w", encoding="utf-8", newline="") as target:
+    for m in get_movies("喜剧"):
+        spamwriter = csv.writer(target, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([m.get_name(), m.get_rate(), m.get_location(),
+                             m.get_category(), m.get_info_link(), m.get_cover_link()])
 
-# with open(sys.path[0] + "\\output.txt", "w", encoding="utf-8") as target:
-#     target.write("测试")
+    for m in get_movies("剧情"):
+        spamwriter = csv.writer(target, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([m.get_name(), m.get_rate(), m.get_location(),
+                             m.get_category(), m.get_info_link(), m.get_cover_link()])
+
+    for m in get_movies("动作"):
+        spamwriter = csv.writer(target, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([m.get_name(), m.get_rate(), m.get_location(),
+                             m.get_category(), m.get_info_link(), m.get_cover_link()])
