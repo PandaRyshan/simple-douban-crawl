0bdc086d22195cce065848dfdd7088562d726944
aold619 <aold619@gmail.com>
2018-01-25 18:37:05 +0800

    refactor code

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -8,7 +8,7 @@ import expanddouban
 from operator import itemgetter
 
 
-def get_movie_url(category="", location=""):
+def getMovieUrl(category="", location=""):
     """ return a string corresponding to the URL of douban movie lists given category and location.
     arguments:
     category -- movie's category str()
@@ -22,103 +22,43 @@ def get_movie_url(category="", location=""):
     return url
 
 
-# html = expanddouban.getHtml(get_movie_url("喜剧", "大陆"), True)
-# print(html)
-
-
 class Movie(object):
-    """ movie class """
-    __name = ""
-    __rate = ""
-    __category = ""
-    __location = ""
-    __info_link = ""
-    __cover_link = ""
+    def __init__(self, name, rate, location, category, info_link, cover_link):
+        self.name = name
+        self.rate = rate
+        self.location = location
+        self.category = category
+        self.info_link = info_link
+        self.cover_link = cover_link
 
-    def __init__(self, name="", rate="", location="", category="", info_link="", cover_link=""):
-        self.__name = name
-        self.__rate = rate
-        self.__location = location
-        self.__category = category
-        self.__info_link = info_link
-        self.__cover_link = cover_link
-
-    def get_name(self):
-        """get name"""
-        return self.__name
-
-    def set_name(self, name):
-        """set name"""
-        self.__name = name
-
-    def get_rate(self):
-        """get rate"""
-        return self.__rate
-
-    def set_rate(self, rate):
-        """set rate"""
-        self.__rate = rate
-
-    def get_location(self):
-        """get location"""
-        return self.__location
-
-    def set_location(self, location):
-        """set location"""
-        self.__location = location
-
-    def get_category(self):
-        """get category"""
-        return self.__category
-
-    def set_category(self, category):
-        """set category"""
-        self.__category = category
-
-    def get_info_link(self):
-        """get info_link"""
-        return self.__info_link
-
-    def set_info_link(self, info_link):
-        """set info_link"""
-        self.__info_link = info_link
-
-    def get_cover_link(self):
-        """get cover_link"""
-        return self.__cover_link
-
-    def set_cover_link(self, cover_link):
-        """set cover_link"""
-        self.__cover_link = cover_link
-
-# m = Movie("name", "rate", "location", "category", "info_link", "cover_link")
-# print(m.get_name())
+    def printData(self):
+        return "{},{},{},{},{},{}".format(self.name, self.rate, self.location, self.category, self.info_link, self.cover_link)
 
 
-def get_movies(category="", location=""):
+def getMovies(category_list="", location_list=""):
     """get the movie list you want
     category -- movie's category str()
     location -- movie's location str()
     """
-    movie_list = []
-    movie = None
-
-    url = get_movie_url(category, location)
-    html = expanddouban.getHtml(url, loadmore=True)
-    soup = BeautifulSoup(html, "html.parser")
-    tag_list = soup.find(id="content").find(class_="list-wp").find_all("a")
-
-    for tag in tag_list:
-        movie = Movie()
-        movie.set_name(str(tag.p.find(class_="title").string))
-        movie.set_rate(str(tag.p.find(class_="rate").string))
-        movie.set_info_link(str(tag.get("href")))
-        movie.set_cover_link(str(tag.find("img").get("src")))
-        movie.set_category(category)
-        movie.set_location(location)
-        movie_list.append(movie)
-
-    return movie_list
+    with open(sys.path[0] + "\\movies.csv", "w", encoding="utf-8", newline="") as target:
+        spamwriter = csv.writer(target, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for cat in category_list:
+            for loc in location_list:
+                html = expanddouban.getHtml(getMovieUrl(cat, loc), True)
+                soup = BeautifulSoup(html, 'html.parser')
+                content_a = soup.find(
+                    class_='list-wp').find_all('a', recursive=False)
+                for element in content_a:
+                    m_name = element.find(class_='title').string
+                    m_rate = element.find(class_='rate').string
+                    m_location = loc
+                    m_category = cat
+                    m_info_link = element.get('href')
+                    m_cover_link = element.find('img').get('src')
+                    spamwriter.writerow(
+                        [m_name, m_rate, m_location, m_category,
+                         m_info_link, m_cover_link, m_cover_link])
 
 
 def get_all_locations():
@@ -126,14 +66,15 @@ def get_all_locations():
     location_list = []
     html = expanddouban.getHtml("https://movie.douban.com/tag")
     soup = BeautifulSoup(html, "html.parser")
-    category_tag_list = soup.find(id="content").find(class_="tags").contents[2].contents
+    category_tag_list = soup.find(id="content").find(
+        class_="tags").contents[2].contents
     for index in range(1, len(category_tag_list)):
         location_list.append(str(category_tag_list[index].span.string))
     return location_list
 
 
 def build_movie_dict():
-    """build two movie subtotal dict"""
+    """read data from movies.csv, build two movie subtotal dict"""
     movie_list = []
     with open(sys.path[0] + "\\movies.csv", "r", encoding="utf-8", newline="") as target:
         reader = csv.reader(target, delimiter=',',
@@ -141,7 +82,7 @@ def build_movie_dict():
         # translate all data to the Movie object list
         movie = None
         for row in reader:
-            movie = Movie(row[0], row[1], row[2], row[3])
+            movie = Movie(row[0], row[1], row[2], row[3], row[4], row[5])
             movie_list.append(movie)
 
     # group movies by category, calculate subtotal
@@ -173,16 +114,7 @@ def build_movie_dict():
 
 
 def analyse_data(tuple_of_dict):
-    """analyse the data
-
-    参考1：https://goo.gl/t3LDxH (python doc -- Sorting HOW TO)
-    参考2：https://goo.gl/Q1JWsf (廖雪峰博客 -- 高阶函数 -- sorted)
-    print(sorted(location_num_dict.items(), key=itemgetter(1), reverse=True))
-    print(sorted(location_num_dict.items(), key=lambda t: t[1], reverse=True))
-    目前还没有理解lambda的使用，如果不使用这两种方法，在本题目中如何给dict排序呢？
-    我开始的思路是用两个list分别存dict的key和value，
-    从value的list中查询排名前三的值的index，这样就能取到对应的key了，但是这样也太蠢了...
-    """
+    """analyse the data"""
     category_num_dict, category_location_dict = tuple_of_dict
     items = category_location_dict.items()
     location_num_dict = None
@@ -193,29 +125,14 @@ def analyse_data(tuple_of_dict):
             location_num_dict = item[1]
             item_num = category_num_dict.get(item[0])
             target.write(str(item[0]) + ": ")
-            result_list = sorted(location_num_dict.items(), key=lambda t: t[1], reverse=True)
+            result_list = sorted(location_num_dict.items(),
+                                 key=lambda t: t[1], reverse=True)
             result_list = result_list[0:3]
             for r in result_list:
                 target.write("({}, {}, {}%)".format(
                     str(r[0]), str(r[1]), round(r[1] / item_num * 100, 2)))
             target.write("\n")
-    print("计算并写入完成")
 
 
-def start_fetch(category_list, location_list):
-    """ fetch data and write data to the csv file
-    argument:
-    category_list -- [str, str, ...]
-    """
-    with open(sys.path[0] + "\\movies.csv", "w", encoding="utf-8", newline="") as target:
-        for category in category_list:
-            for location in location_list:
-                for m in get_movies(category, location):
-                    spamwriter = csv.writer(target, delimiter=',',
-                                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    spamwriter.writerow([m.get_name(), m.get_rate(), m.get_location(),
-                                         m.get_category(), m.get_info_link(), m.get_cover_link()])
-    print("获取数据并写入完成")
-
-# start_fetch(["喜剧", "剧情", "动作"], get_all_locations())
+getMovies(["喜剧", "剧情", "动作"], get_all_locations())
 analyse_data(build_movie_dict())
